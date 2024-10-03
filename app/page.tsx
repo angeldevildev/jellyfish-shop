c6002bf7f2f50d24ed480fadf5e50fb1585156cd
mineshopdev <mineshop47@gmail.com>
2024-10-03 16:52:52 +0200

    Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import GameStore from "./Components/Sidebar";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
    <div>
       <GameStore />
+      <Analytics />
    </div>
   );
 }
